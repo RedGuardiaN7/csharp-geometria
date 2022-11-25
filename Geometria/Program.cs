3848bb97bd6e0e153f95b36e39285a99e4a7b428
RedGuardiaN7 <artiom.str@hotmail.it>
2022-11-25 14:58:23 +0100

    Aggiunta la classe rectangle

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Consegna: chiedere all’utente di inserire, da console, i valori di base e di altezza con cui istanziare un nuovo Rettangolo
+//Dopo averlo istanziato, occupatevi di configurare le sue proprietà di base e altezza e stampare a video il perimetro e l’area
+
+//Viene creato l'oggetto prima di tutto
+
+using System.Drawing;
+
+Rectangle Rectangle_1 = new Rectangle();
+
+Console.WriteLine("Indichi la base del rettangolo...");
+
+string StringUserWidth = Console.ReadLine();
+
+int UserWidth = int.Parse(StringUserWidth);
+
+Rectangle_1.Width = UserWidth;
+
+Console.WriteLine("Indichi l'altezza del rettangolo...");
+
+string StringUserHeight = Console.ReadLine();
+
+int UserHeight = int.Parse(StringUserHeight);
+
+Rectangle_1.Height = UserHeight;
+
+Console.WriteLine(UserHeight + " " + UserWidth);
---
+++ Geometria/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿//Creare una classe Rettangolo con due attributi interi: baseRettangolo e altezzaRettangolo.
+//Aggiungere due metodi: calcolaArea e calcolaPerimetro che calcolano e restituiscono, rispettivamente, l’area e il perimetro del rettangolo.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    public class Rectangle
+    {
+        public int RectangleWidth;
+        public int RectangleHeight;
+
+        public int CalculateArea()
+        {
+            return RectangleWidth * RectangleHeight;
+        }
+
+        public int CalculatePerimeter()
+        {
+            return (RectangleWidth + RectangleHeight) * 2;
+        }
+    }
+}
